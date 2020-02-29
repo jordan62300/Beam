8dba008ab7238fc803af822a2b8eeb2e95084ab6
jordan62300 <39341680+jordan62300@users.noreply.github.com>
2020-02-29 16:13:30 +0100

    Update Connexion_BDD.php

--- models/BDD/Connexion_BDD.php
+++ models/BDD/Connexion_BDD.php
@@ -13,7 +13,7 @@ class Connexion_BDD {
 
 
     // Set the values needed for the connexion
-    public function connexion($db_name="projetBeam",$db_host="localhost",$user='phpmyadmin',$password='f87ac78af'){
+    public function connexion($db_name="projetBeam",$db_host="localhost",$user='',$password=''){
         $this->db_host = $db_host;
         $this->db_name = $db_name;
         $this->user = $user;
